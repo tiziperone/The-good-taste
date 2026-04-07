ede6d6d0812248d2d5ba41bcd01df4cd1c48b791
TizianoPerone <tiziperonetobias@gmail.com>
2026-04-07 20:35:59 -0300

    Se coloco los respectivos nombres a las paginas y el logo de la pagina

--- resources/views/pagina-principal.blade.php
+++ resources/views/pagina-principal.blade.php
@@ -3,7 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="icon" href="{{ asset('Img/Img-Logo.ico') }}" type="image-icon">
   <title>The Good Taste</title>
+
   <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
   <style>
