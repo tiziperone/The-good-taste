07df8b6c7d343e9e227c2bb3f4ec0b032ec86777
TizianoPerone <tiziperonetobias@gmail.com>
2026-04-08 19:49:39 -0300

    Create bondiola.blade.php
    Co-Authored-By: adrianobregon2 <241495190+adrianobregon2@users.noreply.github.com>

---
+++ resources/views/bondiola.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="{{ asset('Img/LogoRedondo.png') }}" type="image-png">
+    <title>The good taste - Bondiola</title>
+
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+</head>
+<body>
+    
+
+    script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
