088324f5302ad69f8c20bf3e5269df59e15126bd
tiziperone <tiziperonetobias@gmail.com>
2026-05-13 20:59:12 -0300

    Se agrego estado a user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->status('active')->default(true);
             $table->string('role')->default('user');
         });
 
