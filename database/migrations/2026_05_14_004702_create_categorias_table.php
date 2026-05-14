3f7972e76570f40357d42e21b37dcbde31736943
tiziperone <tiziperonetobias@gmail.com>
2026-05-13 21:52:50 -0300

    Se creó y migró la tabla Categorias con sus respectivas caracteristicas (o campos)

---
+++ database/migrations/2026_05_14_004702_create_categorias_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombre', 100)->unique();
+            $table->string('descripcion', 255)->nullable();
+            $table->boolean('activo')->default(true);
+            $table->timestamps();   //Crea created_at y updated_at
+            $table->softDeletes();  //Crea deleted_at para borrado lógico
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
