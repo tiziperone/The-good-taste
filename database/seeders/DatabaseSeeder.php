56b1e90d44a21b9c5d41f8681bcdec25bde48f78
tiziperone <tiziperonetobias@gmail.com>
2026-08-26 21:28:15 -0300

    Se corrigieron cambios para que se hacepte js y css

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Test',
+            'apellido' => 'User',
             'email' => 'test@example.com',
         ]);
     }
